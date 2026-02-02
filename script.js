5f114ff38d846cf0cd44670aa5ab894c1e8994be
BkStyle89 <kajzer@hotmail.it>
2026-02-02 15:34:35 +0100

    const km anni prezzo

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Chilometri da percorrere */
+/* età passeggero */
+/* prezzo biglietto 0,21€ x km */
+/* 20% discount minorenni */
+/* 40% discount over 65 */
+
+const km = parseFloat(prompt("quanti chilometri vuoi fare?"))
+const anni = parseFloat(prompt("quanti anni hai?"))
+
+let prezzo = km * 0.21;
+
+if (anni < 18) {
+
+} else if (anni > 65) {
+
+}
